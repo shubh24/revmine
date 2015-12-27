8475f8253071998f386794ad4b11f0fa44f7acce
Shubhankar Srivastava <shubhankar24@gmail.com>
2015-12-28 00:19:11 +0530

    Included scraping Recommended products

--- amazon_rev.py
+++ amazon_rev.py
@@ -16,22 +16,27 @@ db = client.revmine
 reviews = db.reviews
 done = db.done
 queue = db.queue
+#New Collection for Recommended Proucts
+recom = db.recom
 
-
+#queue.insert_one({'_id':'B011RG8SOU'})
 def main():
     """
     While we have Product asin codes in the mongo queue, scrape their product review pages
     and add 'also seen' products to the queue
     """
     while queue.find({}).count > 0:
+        orig_product = queue.find_one()['_id']
         doit()
+        for i in queue.find():
+            recomProd(i['_id'],orig_product)        
 
 
 def doit():
     # picking an object from the queue
     product_asin = queue.find_one()['_id']
     logging.info("Read next value from queue")
-
+        
     logging.info("Start loading reviews")
     li = {}
     li["_id"] = product_asin
@@ -45,7 +50,7 @@ def doit():
     # will scrape reviews' text
     for j, row in enumerate(soup('span', {'class': 'review-text'})):
         li[str(j + 1)] = row.text
-
+    
     logging.info("adding entries to queue")
 
     for div in soup('div', {'class': 'description'}):
@@ -70,8 +75,37 @@ def doit():
 
     logging.info("removing entry from queue")
     queue.remove({"_id": product_asin}, 1)
+    
+        
 
     assert(inserted_review == product_asin)
+
+
+def recomProd(product_asin, orig_product):
+    
+    li = {}
+    li["_id"] = product_asin
+    li["recom_by"] = orig_product
+
+    #Getting the title
+    url_ = amazon_link % (product_asin, 1)
+    soup = BeautifulSoup(urlopen(url_).read())
+    li['title'] = soup('span', {'class': 'a-text-ellipsis'})[0].a.text
+
+    #Extracting the reviews
+    for i in range(1,6,1): 
+        url_ = amazon_link % (product_asin, i)
+        print "Trying " + url_ + " now!"
+        soup = BeautifulSoup(urlopen(url_).read())
+        for j, row in enumerate(soup('span', {'class': 'review-text'})):
+            li[str(j +10*(i-1))] = row.text
+
+    #Inserting into the Recom collection
+    inserted_review = recom.insert_one(li).inserted_id
+    logging.info("reviews loaded into recom for " + li['title'])
+
+    logging.info("removing entry from queue")
+    queue.remove({"_id": product_asin}, 1)
 
 if __name__ == '__main__':
     main()
